c788c422e62a5491f61c6ae445a6b2e40a9775fb
Nikita Verma <89023534+Nikitaverma2608@users.noreply.github.com>
2022-04-23 09:14:43 +0530

    Add files via upload

---
+++ string_manipulation.py
@@ -0,0 +1,12 @@
+#String manipulation
+"""a = input("Enter your name\n")
+print(a)
+for i in a:
+    print(i) """
+
+
+"""a = input("Enter your name\n")
+print(a)
+print(a[0])
+print(a[1])
+print(a[2])"""
