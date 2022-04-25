0fa436ebbd32ba915e99fbf89a20b01be9ef167b
Nikita Verma <89023534+Nikitaverma2608@users.noreply.github.com>
2022-04-25 09:54:59 +0530

    Add files via upload

---
+++ Prime_number.py
@@ -0,0 +1,12 @@
+#To check whether the number is primary or not
+
+num = int(input("Enter a number : "))
+
+if num==1 or num==2:
+    print(num , "is a Prime number. ")
+elif num%2==0:
+    print(num, "is a not Prime number. ")
+else:
+    print(num , "is a Prime number. ")
+    
+        
