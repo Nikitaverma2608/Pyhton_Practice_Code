f31a31f31061a2e5d33cca6aa9e436f99d44cf91
Nikita Verma <89023534+Nikitaverma2608@users.noreply.github.com>
2022-04-27 18:17:34 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,7 @@
+from array import *
+x = array('I',[1,2,3,4,5])
+print(x)
+print(type(x))
+
+#in python it is not possible to make multidimentional array so we have to install other file using pip install called numpy
+
