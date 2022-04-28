ea08776c6cf6350f437dc7452cbd40643d707700
Nikita Verma <89023534+Nikitaverma2608@users.noreply.github.com>
2022-04-28 19:05:18 +0530

    Add files via upload

---
+++ classes_intro.py
@@ -0,0 +1,10 @@
+class car:        #class definition
+     def __init__(self,b_name,model,type):
+
+         self.b_name=b_name     #function definition
+         self.model=model
+         self.type=type
+
+
+car1=car("MARUTI","CIAZ","SEDAN")   #variable initialization
+print(car1.model)
