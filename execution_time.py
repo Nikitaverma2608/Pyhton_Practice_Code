e444c5bedfd52806a86819a6ed1d32ece9135666
Nikita Verma <89023534+Nikitaverma2608@users.noreply.github.com>
2022-05-03 21:44:02 +0530

    Add files via upload

---
+++ execution_time.py
@@ -0,0 +1,31 @@
+f = open("To calculate execution time.txt","w")
+
+x1="time for iteration method for fact of 5 = 1648789751.7785525 to 1648789751.779553  "
+f.write(x1)
+f.write("\n")
+y1="time for recursion method for fact of 5 = 1648789906.4983451 to 1648789906.4998453  "
+f.write(y1)
+f.write("\n")
+
+x1="time for iteration method for fact of 10 =  1648790132.5746624 to 1648790132.5746624  "
+f.write(x1)
+f.write("\n")
+y1="time for recursion method for fact of 10 = 1648790047.3501332 to 1648790047.3501332 "
+f.write(y1)
+f.write("\n")
+
+x1="time for iteration method for fact of 20 = 1648790280.7608163 to 1648790280.7608163  "
+f.write(x1)
+f.write("\n")
+y1="time for recursion method for fact of 20 = 1648790332.034415 to 1648790332.034415 "
+f.write(y1)
+f.write("\n")
+
+x1="time for iteration method for fact of 995 = 1648790501.0828733 to 1648790501.091378  "
+f.write(x1)
+f.write("\n")
+y1="time for recursion method for fact of 995 = 1648790407.5439854 to 1648790407.5519342 "
+f.write(y1)
+f.write("\n")
+
+f.close()
